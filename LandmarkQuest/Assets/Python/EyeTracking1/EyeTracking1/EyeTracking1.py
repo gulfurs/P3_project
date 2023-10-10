50a48fe4c1dc7df88088b927199ed10829702136
Mufasakong <Halipha.haidary@gmail.com>
2023-10-10 17:18:25 +0200

    The OG Eyetracking
    Remove hashtag from webcam.release() if you want video

--- LandmarkQuest/Assets/Python/EyeTracking1/EyeTracking1/EyeTracking1.py
+++ LandmarkQuest/Assets/Python/EyeTracking1/EyeTracking1/EyeTracking1.py
@@ -1,7 +1,9 @@
 import cv2
 from gaze_tracking import GazeTracking
 import socket
+import pickle
 import struct
+import zlib
 import threading
 
 # Initialize GazeTracking and webcam
@@ -40,6 +42,29 @@ def handle_client(client_socket):
                     print("Socket error: " + str(e))
                     clients.remove(c)
 
+            # Continue displaying gaze-related information locally
+            text = ""
+            if is_blinking:
+                text = "Blinking"
+            elif gaze.is_right():
+                text = "Looking right"
+            elif gaze.is_left():
+                text = "Looking left"
+            elif gaze.is_center():
+                text = "Looking center"
+
+            cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+            left_pupil = gaze.pupil_left_coords()
+            right_pupil = gaze.pupil_right_coords()
+            cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+            cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+            #cv2.imshow("Demo", frame)
+
+            if cv2.waitKey(1) == 27:
+                break
+
     except Exception as e:
         print("Client disconnected: " + str(e))
         client_socket.close()
@@ -55,7 +80,10 @@ while True:
     client_handler.start()
 
 # Release the webcam resources
-# webcam.release()
+#webcam.release()
 
 # Clean up and close the server socket
 server_socket.close()
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
