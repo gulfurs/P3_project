659ce49e9449441e09ee34d4adfacf172a2d9ab9
fijibottle <gulfurs@gmail.com>
2023-11-01 15:42:58 +0100

    i see no changes
    I wake up in the morning

---
+++ LandmarkQuest/Assets/_scripts/Python/TestScript/Pythonrunner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEditor.Scripting.Python;
+
+public class Pythonrunner : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Unity: Hello World");
+        print("Python: Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
