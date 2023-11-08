5bc9af19bb4842be979096526d28c94e500cb3db
Simon <gulfurs@gmail.com>
2023-11-08 14:00:14 +0100

    update to test_4

--- Own_Python_EyeTrack/Test_4.py
+++ Own_Python_EyeTrack/Test_4.py
@@ -1,34 +1,50 @@
 import cv2
 import mediapipe as mp
 import pyautogui
-cam = cv2.VideoCapture(0)
-face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+
+#initialize camera
+cam = cv2.VideoCapture(0) #use 0 for the default camera
+
+#face Mesh model via mediapipe
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True) #Gets the face mash
+
 screen_w, screen_h = pyautogui.size()
+
 while True:
     _, frame = cam.read()
     frame = cv2.flip(frame, 1)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    #process the frame with Face Mesh model
     output = face_mesh.process(rgb_frame)
+
+    #extract face landmarks
     landmark_points = output.multi_face_landmarks
     frame_h, frame_w, _ = frame.shape
+
     if landmark_points:
         landmarks = landmark_points[0].landmark
+
+                
         for id, landmark in enumerate(landmarks[474:478]):
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 0))
+                
             if id == 1:
                 screen_x = screen_w * landmark.x
                 screen_y = screen_h * landmark.y
                 pyautogui.moveTo(screen_x, screen_y)
-        left = [landmarks[145], landmarks[159]]
-        for landmark in left:
-            x = int(landmark.x * frame_w)
-            y = int(landmark.y * frame_h)
-            cv2.circle(frame, (x, y), 3, (0, 255, 255))
-        if (left[0].y - left[1].y) < 0.004:
-            pyautogui.click()
-            pyautogui.sleep(1)
+
+                if screen_x < screen_w / 3:
+                    print("Left")
+                elif screen_x > 2 * screen_w / 3:
+                    print("Right")
+
     cv2.imshow('Eye Controlled Mouse', frame)
+
     if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
         break
+
+cam.release()
+cv2.destroyAllWindows()
