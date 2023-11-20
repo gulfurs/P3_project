fc9db615422ac01b9c8ee33d6dfcf8ec44c52954
Simon <gulfurs@gmail.com>
2023-11-20 10:57:48 +0100

    righteye

--- Own_Python_EyeTrack/Test_4.py
+++ Own_Python_EyeTrack/Test_4.py
@@ -25,21 +25,21 @@ while True:
     if landmark_points:
         landmarks = landmark_points[0].landmark
 
-                
-        for id, landmark in enumerate(landmarks[474:478]):
+        right = [landmarks[374], landmarks[387]]
+        for landmark in right:
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
-            cv2.circle(frame, (x, y), 3, (0, 255, 0))
-                
-            if id == 1:
-                screen_x = screen_w * landmark.x
-                screen_y = screen_h * landmark.y
-                pyautogui.moveTo(screen_x, screen_y)
+            cv2.circle(frame, (x, y), 3, (255, 0, 0))
+        if (right[0].y - right[1].y) < 0.008:
+            print("test right")
 
-                if screen_x < screen_w / 3:
-                    print("Left")
-                elif screen_x > 2 * screen_w / 3:
-                    print("Right")
+        left = [landmarks[145], landmarks[159]]
+        for landmark in left:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        if (left[0].y - left[1].y) < 0.008:
+            print("test left")
 
     cv2.imshow('Eye Controlled Mouse', frame)
 
