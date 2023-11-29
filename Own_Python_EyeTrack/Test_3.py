059cd327f52a9e35db74e87a79c8fa24b2108439
fijibottle <gulfurs@gmail.com>
2023-11-29 12:52:38 +0100

    update test3
    its now a headtilt without sockets
    that is better

--- Own_Python_EyeTrack/Test_3.py
+++ Own_Python_EyeTrack/Test_3.py
@@ -1,99 +1,72 @@
-#This is all from here https://github.com/stepacool/Eye-Tracker/blob/No_GUI/track.py
-#And dont work
-
 import cv2
-import numpy as np
+import mediapipe as mp
+import pyautogui
+import socket
+import struct
 
+# Initialize camera
+cam = cv2.VideoCapture(0)  # Use 0 for the default camera
 
-# init part
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-detector_params = cv2.SimpleBlobDetector_Params()
-detector_params.filterByArea = True
-detector_params.maxArea = 1500
-detector = cv2.SimpleBlobDetector_create(detector_params)
+# Face Mesh model via mediapipe
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)  # Gets the face mesh
 
+screen_w, screen_h = pyautogui.size()
 
-def detect_faces(img, cascade):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    coords = cascade.detectMultiScale(gray_frame, 1.3, 5)
-    if len(coords) > 1:
-        biggest = (0, 0, 0, 0)
-        for i in coords:
-            if i[3] > biggest[3]:
-                biggest = i
-        biggest = np.array([i], np.int32)
-    elif len(coords) == 1:
-        biggest = coords
-    else:
-        return None
-    for (x, y, w, h) in biggest:
-        frame = img[y:y + h, x:x + w]
-    return frame
+# Create a window, but do not display it
+cv2.namedWindow('Test4', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('Test4', cv2.WND_PROP_VISIBLE, cv2.WINDOW_NORMAL)
 
+current_state = "Center"
+pre_state = "Center"
 
-def detect_eyes(img, cascade):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    eyes = cascade.detectMultiScale(gray_frame, 1.3, 5)  # detect eyes
-    width = np.size(img, 1)  # get face frame width
-    height = np.size(img, 0)  # get face frame height
-    left_eye = None
-    right_eye = None
-    for (x, y, w, h) in eyes:
-        if y > height / 2:
-            pass
-        eyecenter = x + w / 2  # get the eye center
-        if eyecenter < width * 0.5:
-            left_eye = img[y:y + h, x:x + w]
+while True:
+    _, frame = cam.read()
+    frame = cv2.flip(frame, 1)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Process the frame with Face Mesh model
+    output = face_mesh.process(rgb_frame)
+
+    # Extract face landmarks
+    landmark_points = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+
+    if landmark_points:
+        landmarks = landmark_points[0].landmark
+
+        central_point = landmarks[10]
+        x = int(central_point.x * frame_w)
+        y = int(central_point.y * frame_h)
+        cv2.circle(frame, (x, y), 3, (0, 255, 0))
+
+        screen_x = screen_w * central_point.x
+        screen_y = screen_h * central_point.y
+
+        threshold1 = int(screen_w / 4)
+        threshold2 = int(screen_w / 8)
+
+        cv2.line(frame, (threshold1, 0), (threshold1, screen_h), (255, 0, 0), 2)
+        cv2.line(frame, (threshold2, 0), (threshold2, screen_h), (255, 0, 0), 2)
+
+        if screen_x < screen_w / 3:
+            #print("Left")
+            current_state = "Left"
+        elif screen_x > 2 * screen_w / 3:
+            #print("Right")
+            current_state = "Right"
         else:
-            right_eye = img[y:y + h, x:x + w]
-    return left_eye, right_eye
+            #print("Center")
+            current_state = "Center"
 
+        if current_state != pre_state:
+            print(current_state)
 
-def cut_eyebrows(img):
-    height, width = img.shape[:2]
-    eyebrow_h = int(height / 4)
-    img = img[eyebrow_h:height, 0:width]  # cut eyebrows out (15 px)
+        pre_state = current_state 
 
-    return img
+    cv2.imshow('Test4', frame)
 
+    if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
+        break
 
-def blob_process(img, threshold, detector):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
-    #img = cv2.erode(img, None, iterations=2)
-    #img = cv2.dilate(img, None, iterations=4)
-    #img = cv2.medianBlur(img, 5)
-    keypoints = detector.detect(img)
-    print(keypoints)
-    return keypoints
-
-
-def nothing(x):
-    pass
-
-
-def main():
-    cap = cv2.VideoCapture(0)
-    cv2.namedWindow('image')
-    cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
-    while True:
-        _, frame = cap.read()
-        face_frame = detect_faces(frame, face_cascade)
-        if face_frame is not None:
-            eyes = detect_eyes(face_frame, eye_cascade)
-            for eye in eyes:
-                if eye is not None:
-                    threshold = r = cv2.getTrackbarPos('threshold', 'image')
-                    eye = cut_eyebrows(eye)
-                    keypoints = blob_process(eye, threshold, detector)
-                    eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv2.imshow('image', frame)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    main()
+cam.release()
+cv2.destroyAllWindows()
